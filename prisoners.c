ac7d3e72445937b75d8b0222c6514ac7bbac46cc
Luis Colorado <luiscoloradourcola@gmail.com>
2022-09-19 00:31:24 +0300

    Change the permutation selection algorithm
    Now equal probability of even or odd permutation
    
    Signed-off-by: Luis Colorado <luiscoloradourcola@gmail.com>

--- prisoners.c
+++ prisoners.c
@@ -10,88 +10,96 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-#define FLG_TRACE (1 << 0)
+#define FLG_QUIET (1 << 0)
+#define RANDOM(n) ((unsigned)random() % (n))
 
 int flags = 0;
 
-int experiment(int n)
+unsigned experiment(unsigned n)
 {
-    int *box = calloc(n, sizeof *box),
-         n2  = n >> 1;
+    unsigned *box  = calloc(n, sizeof *box),
+              n2   = n >> 1;
     uint64_t *bits = calloc((n + 63) >> 6, sizeof *bits);
 
     for (unsigned i = 0; i < n; i++) {
-        box[i] = i;
+         box[i] = i;
     }
-    if (flags & FLG_TRACE)
-        printf("\n");
 
     /* random permutation */
-    char *sep = "";
-    for (int i = 0; i < n; i++) {
-        int j = i + random() % (n - i);
+    for (unsigned i = 0, j; i < n; i++) {
+        j = RANDOM(n);
         if (i != j) {
-            int temp = box[i];
-            box[i] = box[j];
-            box[j] = temp;
+            unsigned
+				temp = box[i];
+              box[i] = box[j];
+              box[j] = temp;
         }
-        if (flags & FLG_TRACE) {
-            printf("%s%d->%d", sep, i+1, box[i]+1);
-            sep = ", ";
+	} /* for */
+
+	/* print permutation */
+	if (!(flags & FLG_QUIET)) {
+		char *sep  = "";
+		for (unsigned i = 0; i < n; i++) {
+			printf("%s[%d]->%d", sep, i + 1, box[i] + 1);
+			sep = ", ";
         }
-    }
-    if (flags & FLG_TRACE)
         printf("\n");
+    }
 
     /* the experiment */
-    int ret_val = 0;
-    for (int i = 0; i < n; i++) {
+    unsigned ret_val = 0;
+    for (unsigned i = 0; i < n; i++) {
         if (bits[i >> 6] & (1UL << (i & 63)))
             continue;
-        int k = 1;
-        if (flags & FLG_TRACE)
-            printf("(%d", i+1);
-        for (int j = box[i]; j != i; j = box[j], k++) {
+        unsigned k = 1;
+        if (!(flags & FLG_QUIET))
+            printf("(%d", i + 1);
+        for (unsigned j = box[i]; j != i; j = box[j], k++) {
             bits[j >> 6] |= (1UL << (j & 63));
-            if (flags & FLG_TRACE)
-                printf(", %d", j+1);
+            if (!(flags & FLG_QUIET))
+                printf(", %d", j + 1);
         }
-        if (flags & FLG_TRACE)
-            printf(")%s\n", k <= n2 ? "*" : "?");
+        if (!(flags & FLG_QUIET))
+            printf(")%c\n", k <= n2
+					? '*'
+					: '?');
         if (k <= n2)
             ret_val += k;
-    }
+    } /* for */
     free(box);
     free(bits);
     return ret_val;
-}
+} /* experiment */
 
 int main(int argc, char **argv)
 {
-    int N    = 100,
+    unsigned
+		N    = 100,
         ITER =   1,
         opt;
 
-    while ((opt = getopt(argc, argv, "di:n:")) != EOF) {
+    while ((opt = getopt(argc, argv, "i:n:q")) != EOF) {
         switch (opt) {
         case 'n': N      = atoi(optarg); break;
         case 'i': ITER   = atoi(optarg); break;
-        case 'd': flags |= FLG_TRACE;    break;
+        case 'q': flags |= FLG_QUIET;    break;
         }
     } /* while */
 
     srandomdev();
-    int N2    = N >> 1,
-        num   =      0,
+    unsigned
+		N2    = N >> 1,
         denom =   ITER,
-        i;
+		num   =      0;
+
     printf("Calc. %d times\n", ITER);
-    for (i = 0; i < denom; i++)
+
+    for (unsigned i = 0; i < denom; i++)
         if (experiment(N) == N)
             num++;
 
     printf("Estim. Montecarlo: %d/%d ~= %0.10lf\n",
-            num, denom, (double)num / denom);
+            num, denom, (double) num / denom);
 
     /* calculation of 1 - sum(i; n2; n; 1/i) */
 
