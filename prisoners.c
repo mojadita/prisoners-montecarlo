14460f632ba4942c5d7caaae0bbeb4110bce3019
Luis Colorado <luiscoloradourcola@gmail.com>
2022-09-25 11:27:05 +0300

    Add number of prisoners to header output
    Signed-off-by: Luis Colorado <luiscoloradourcola@gmail.com>

--- prisoners.c
+++ prisoners.c
@@ -92,7 +92,7 @@ int main(int argc, char **argv)
         denom =   ITER,
 		num   =      0;
 
-    printf("Calc. %d times\n", ITER);
+    printf("Calc. %u times, %u prisoners\n", ITER, N);
 
     for (unsigned i = 0; i < denom; i++)
         if (experiment(N) == N)
